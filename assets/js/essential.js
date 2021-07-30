62ad0e38202dde4e1e7114dc461649686979361f
seed-info <info@seed-info.com>
2021-07-30 17:45:01 +0900

    $2021년 7월 30일 금요일 17시 45분 01초 KST

--- assets/js/essential.js
+++ assets/js/essential.js
@@ -1,3 +1,31 @@
+function CopyUrlToClipboard(){
+    var dummy   = document.createElement("input");
+    var text    = decodeURIComponent(location.href);
+    
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+    alert('복사되었습니다.\n원하시는 곳에 붙여넣기 하세요');
+}
+
+function copyStringToClipboard (str) {
+    // Create new element
+    var el = document.createElement('textarea');
+    // Set value (string to be copied)
+    el.value = str;
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '');
+    el.style = {position: 'absolute', left: '-9999px'};
+    document.body.appendChild(el);
+    // Select text inside element
+    el.select();
+    // Copy text to clipboard
+    document.execCommand('copy');
+    // Remove temporary element
+    document.body.removeChild(el);
+ }
 
 Date.prototype.format = function(f) {
     if (!this.valueOf()) return " ";
